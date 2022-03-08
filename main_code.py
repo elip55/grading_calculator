3680591800e77799715bcc92c7da969f94a1477d
Eli Pacheco <77797720+elip55@users.noreply.github.com>
2022-03-08 16:40:39 -0700

    Update main_code.py

--- main_code.py
+++ main_code.py
@@ -1,6 +1,3 @@
-# make a program that calculates your final grade in a class based on overall points
-# first draft
-
 class Calculation:
     
     def __init__(self, range1, v1, v2, v3, v4):
@@ -12,29 +9,29 @@ class Calculation:
         
     def calc_percentage(self):
         grade = (self.v1/self.v2)*100
-        return grade
+        final_grade = round(grade, 2)
+        return final_grade
 
-            
-        
+# ----------------------------------------------
 
 ticker = 0
 counter = '*'
 names_and_points = {}
-random = int(input("How many inputs to you have? "))
+print('We will split up each point structure into "categories".  Such as exams, hw, final, etc.')
+random = int(input("How many categories do you want to calculate?\nInput a number: "))
 loop_range = random*counter
-
+user_points = []
 for i in range(len(loop_range)):
-    name = input('What is the category you want to calculate? ')
-    points = float(input(f'Input total points of {name}: '))
+    ticker += 1
+    name = input(f'Please input the name of category {ticker}: ')
+    points = float(input(f'Input total points possible of {name}: '))
+    user_attained = float(input(f'How many points have you attained on {name}: '))
     names_and_points[name] = points 
+    user_points.append(user_attained)
 
 total_points = sum(names_and_points.values())
-user_points = []
-for i in names_and_points.keys():
-    user_attained = float(input(f'What points have you attained on {i}: '))
-    user_points.append(user_attained)
 total_attained_points = sum(user_points)
 
 final = Calculation(0, total_attained_points, total_points, 0, 0)
 final_grade = final.calc_percentage()
-print(final_grade)
+print(f'Your current/final grade is: {final_grade}')
